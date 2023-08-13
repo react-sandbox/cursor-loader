d33798b8ca9ec731ab1318ee1fb769d381d3df74
William Grosset <williamhgrosset@gmail.com>
2023-08-13 00:41:27 -0600

    Restrict load range between 0 and 100 (inclusive)

--- src/types/CursorLoader.d.ts
+++ src/types/CursorLoader.d.ts
@@ -1,8 +1,20 @@
+type Enumerate<
+  N extends number,
+  Acc extends number[] = []
+> = Acc['length'] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc['length']]>
+
+type IntRange<F extends number, T extends number> = Exclude<
+  Enumerate<T>,
+  Enumerate<F>
+>
+
 type Size = 'sm' | 'md' | 'lg'
 type Delay = 0 | 100 | 200 | 300 | 500
 
 export interface CursorLoaderProps {
-  load: number
+  load: IntRange<0, 100>
   color?: string
   size?: Size
   trailDelay?: Delay
