1377a9fcbf8fd240dce620c68ce3cc234bbddd1c
William Grosset <williamhgrosset@gmail.com>
2023-08-16 01:13:24 -0600

    Add playwright workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ import { defineConfig, devices } from '@playwright/test'
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './test',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -31,6 +31,7 @@ export default defineConfig({
   webServer: {
     command: 'yarn run dev',
     url: 'http://127.0.0.1:3000',
+    cwd: './test',
     reuseExistingServer: !process.env.CI
   },
   /* Configure projects for major browsers */
