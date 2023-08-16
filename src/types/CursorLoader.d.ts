ef306fa6b99c600a6521ce0daccd1eafa8120568
William Grosset <williamhgrosset@gmail.com>
2023-08-15 18:00:43 -0600

    Remove specific load range

--- src/types/CursorLoader.d.ts
+++ src/types/CursorLoader.d.ts
@@ -1,20 +1,8 @@
-type Enumerate<
-  N extends number,
-  Acc extends number[] = []
-> = Acc['length'] extends N
-  ? Acc[number]
-  : Enumerate<N, [...Acc, Acc['length']]>
-
-type IntRange<F extends number, T extends number> = Exclude<
-  Enumerate<T>,
-  Enumerate<F>
->
-
 type Size = 'sm' | 'md' | 'lg'
 type Delay = 0 | 100 | 200 | 300 | 500
 
 export interface CursorLoaderProps {
-  load: IntRange<0, 100>
+  load: number
   color?: string
   size?: Size
   trailDelay?: Delay
